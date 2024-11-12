3fb1ce3d2f8847aca3006ab16a476fe8511462bf
antho <anthonynuge0509@gmail.com>
2024-11-11 20:59:05 -0600

    Add logic for checking if model is available and make if needed.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import os
 from src.download_dataset import download_dataset
 import tkinter as tk
 from src.gui import FruitClassifierGui
+
+MODELS_DIR = './models'
+
+def available_model(path=MODELS_DIR):
+    """Check if there is a model trained and ready. First application boot model will be trained using data"""
+    return any(os.path.isfile(os.path.join(path, m) for m in os.listdir(path)))
+
 
 if __name__ == "__main__":
 
     # if no model download and train the model
 
     app = FruitClassifierGui()
-
-
